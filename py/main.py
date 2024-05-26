20a597326e2b6ee9a08a092f3b8b2f460f38b517
tudor-c <tudor.copilu@gmail.com>
2024-05-26 17:27:28 +0300

    shift register fix

--- py/main.py
+++ py/main.py
@@ -1,24 +1,36 @@
 #!/bin/python3
 import RPi.GPIO as GPIO
-import shift_register as SR
+from shift_register import SN74HC595
 import time
 
-GPIO.setmode(GPIO.BOARD)
 
 def main():
     print("Labirint din LED-uri - proiect SI")
 
+    GPIO.setmode(GPIO.BOARD)
+
+    shift_reg1 = SN74HC595(latch_clk=11, shift_clk=12, data_bit=13)
+    shift_reg2 = SN74HC595(latch_clk=15, shift_clk=16, data_bit=18)
+
     while True:
         temp = 1
-        for i in range(0, 8):
-            SR.writeOut(temp)
+        for i in range(0, 16):
+            print(i)
+            if i < 8:
+                shift_reg1.writeOut(temp)
+                shift_reg2.writeOut(0)
+            else:
+                shift_reg1.writeOut(0)
+                shift_reg2.writeOut(temp)
             temp <<= 1
+            if temp > 255:
+                temp = 1
             time.sleep(0.2)
 
-        for i in range(0, 8):
-            temp >>= 1
-            SR.writeOut(temp)
-            time.sleep(0.2)
+        # for i in range(0, 8):
+        #     temp >>= 1
+        #     shift_reg1.writeOut(temp)
+        #     time.sleep(0.2)
 
 
 if __name__ == '__main__':
--- py/shift_register.py
+++ py/shift_register.py
@@ -1,38 +1,43 @@
 #!/bin/python3
 
 import RPi.GPIO as GPIO
-# import time
+import time
 
-LATCH = 11
-CLK = 12
-DATA_BIT = 7
+# LATCH = 11
+# CLK = 12
+# DATA_BIT = 7
 
-GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(LATCH, GPIO.OUT)
-GPIO.setup(CLK, GPIO.OUT)
-GPIO.setup(DATA_BIT, GPIO.OUT)
+class SN74HC595:
+    def __init__(self, latch_clk, shift_clk, data_bit):
+        self.latch_clk = latch_clk
+        self.shift_clk = shift_clk
+        self.data_bit = data_bit
 
-GPIO.output(LATCH, 0)
-GPIO.output(CLK, 0)
+        GPIO.setup(self.latch_clk, GPIO.OUT)
+        GPIO.setup(self.shift_clk, GPIO.OUT)
+        GPIO.setup(self.data_bit, GPIO.OUT)
 
-def _shiftRegister():
-    GPIO.output(CLK, 1)
-    # time.sleep(0.001)
-    GPIO.output(CLK, 0)
+        GPIO.output(self.latch_clk, 0)
+        GPIO.output(self.shift_clk, 0)
 
-def _updateLatch():
-    GPIO.output(LATCH, 1)
-    # time.sleep(0.001)
-    GPIO.output(LATCH, 0)
+    def _shiftRegister(self):
+        GPIO.output(self.shift_clk, 1)
+        time.sleep(0.001)
+        GPIO.output(self.shift_clk, 0)
 
-def writeOut(value):
-    for x in range(0, 8):
-        temp = value & 0x80
-        if temp == 0x80:
-            GPIO.output(DATA_BIT, 1)
-        else:
-            GPIO.output(DATA_BIT, 0)
-        _shiftRegister()
-        value <<= 1
-    _updateLatch()
+    def _updateLatch(self):
+        GPIO.output(self.latch_clk, 1)
+        time.sleep(0.001)
+        GPIO.output(self.latch_clk, 0)
+
+    def writeOut(self, value):
+        for x in range(0, 8):
+            temp = value & 0x80
+            if temp == 0x80:
+                GPIO.output(self.data_bit, 1)
+            else:
+                GPIO.output(self.data_bit, 0)
+            self._shiftRegister()
+            value <<= 1
+        self._updateLatch()
