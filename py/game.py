aa2c7c45355f2a3397c90d3306afad56dd3f6fe9
tudor-c <tudor.copilu@gmail.com>
2024-05-28 01:12:12 +0300

    finished game

--- py/game.py
+++ py/game.py
@@ -12,14 +12,38 @@ class Point:
     x: int
     y: int
 
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
 
 class Game:
     _LEVELS = [
         """ .x..x..x
             .xx.xx..
             .....x.x
-            .x.x.x.x
-            ...x.... """,
+            .x.x...x
+            ...x.x.. """,
+
+        """ ....x...
+            .xx.x.x.
+            ..x...x.
+            x...x.x.
+            ..x..... """,
+
+        """ ........
+            xxxxx.x.
+            ........
+            .xxxxx.x
+            ........ """,
+
+        """ .xx.....
+            ....x.x.
+            x.xxx.x.
+            x.x.xxx.
+            x....... """,
     ]
     _HEIGHT = 5
     _WIDTH = 8
@@ -29,10 +53,11 @@ class Game:
         self.level = None
         self.player_pos = None
         self.enemy_pos = None
+        self._init()
 
     def _init(self):
         level_data = [line.strip() for line in random.choice(Game._LEVELS).splitlines()]
-        self.level = convertDataToGrid(level_data, Color.ALL)
+        self.level = convertDataToGrid(level_data, Color.GREEN)
         self.player_pos = Point(0, 0)
         self.enemy_pos = Point(Game._WIDTH - 1, Game._HEIGHT - 1)
 
@@ -47,6 +72,8 @@ class Game:
 
         if print_players:
             display[self.player_pos.y][self.player_pos.x] = Color.ALL
+            if int(time.time() * 10) % 7 != 0:
+                display[self.enemy_pos.y][self.enemy_pos.x] = Color.RED
 
         for i in range(Game._HEIGHT):
             activateLine(i, display[i])
@@ -62,7 +89,7 @@ class Game:
         while time.time() - start < duration:
             self._printGrid(letter, False)
 
-    def _printText(self, text: str, color: Color, letter_duration=0.4):
+    def _printText(self, text: str, color: Color = Color.ALL, letter_duration: float =0.4):
         for c in text:
             self._printLetter(c, color, letter_duration)
         self._clearScreen()
@@ -71,7 +98,12 @@ class Game:
         clear_grid = [[Color.OFF for _ in range(Game._WIDTH)] for _ in range(Game._HEIGHT)]
         self._printGrid(clear_grid, False)
 
-    def _movePlayer(self):
+    def _movePlayer(self) -> bool:
+        """
+        Citeste input-ul de la butoane si muta jucatorul in caz de apasare.
+
+        :return: True if player moved, Flase otherwise
+        """
         x = self.player_pos.x
         y = self.player_pos.y
 
@@ -79,29 +111,85 @@ class Game:
             new_pos = Point(x - 1, y)
             if self._isValid(new_pos):
                 self.player_pos = new_pos
+                return True
         if isPressed(Buttons.RIGHT):
             new_pos = Point(x + 1, y)
             if self._isValid(new_pos):
                 self.player_pos = new_pos
+                return True
         if isPressed(Buttons.UP):
             new_pos = Point(x, y - 1)
             if self._isValid(new_pos):
                 self.player_pos = new_pos
+                return True
         if isPressed(Buttons.DOWN):
             new_pos = Point(x, y + 1)
             if self._isValid(new_pos):
                 self.player_pos = new_pos
+                return True
+        return False
+
+    def _moveEnemy(self):
+        dirs = [
+            Point(-1, 0),  # left
+            Point(0, -1),  #up
+            Point(1, 0),  # right
+            Point(0, 1),  # down
+        ]
+        random.shuffle(dirs)
+        for direction in dirs:
+            new_pos = self.enemy_pos + direction
+            if self._isValid(new_pos):
+                self.enemy_pos = new_pos
+                break
+
+    def _playerLost(self) -> bool:
+        if self.player_pos == self.enemy_pos:
+            return True
+        elif (abs(self.player_pos.x - self.enemy_pos.x) +
+              abs(self.player_pos.y - self.enemy_pos.y) == 0):
+            return True
+        return False
+
+    def _playerWon(self) -> bool:
+        if (self.player_pos == Point(Game._WIDTH - 1, Game._HEIGHT - 1) and
+            self.player_pos != self.enemy_pos):
+            return True
+        return False
+
+    def _checkRestart(self):
+        """
+        Reporneste jocul daca se apasa tasta ENTER
+        """
+        if isPressed(Buttons.ENTER):
+            self._clearScreen()
+            self.start()
 
     def start(self):
+        time.sleep(1)
         self._init()
-        # self._printText("START", Color.BOTH)
+        self._printText("START")
         while True:
-            if isPressed(Buttons.ENTER):
-                # reia jocul
-                self.start()
-                continue
+            self._checkRestart()
+            moved = self._movePlayer()
 
-            self._movePlayer()
+            if self._playerWon():
+                self._clearScreen()
+                time.sleep(1)
+                self._printText("AI CASTIGAT")
+                time.sleep(1)
+                self.start()
+
+            if self._playerLost():
+                self._clearScreen()
+                time.sleep(1)
+                self._printText("AI PIERDUT")
+                time.sleep(1)
+                self.start()
+
+            if moved:
+                self._moveEnemy()
+
             self._printGrid(self.level, True)
 
 
